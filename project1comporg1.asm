84519bd1836084c338b5eaf2a92851af442db1a0
Jenaba Sow <jenaba@Jenabas-MacBook-Pro.local>
2018-10-17 23:47:06 -0400

    added veritcal tab, @ sign and matched numbers up so the math made snese going from tab to last name to first name

--- project1comporg1.asm
+++ project1comporg1.asm
@@ -1,13 +1,17 @@
+
 .text
-	main: 	
-		li $v0, 11 #system call code tp print characters 
- 		la $a0, 83 #S (83)
+	main: 
+		li $v1, 11 #system call code tp print characters
+		la $a0, 64 #@ 
+		sub $a0, $a0, 53 #(11 =  vertical tab or maybe i should use 9 = new line feed)
+		syscall  
+ 		addi $a0, $a0, 72 #S (83)
 		syscall
 		addi $a0, $a0, 28 #o (111)
 		syscall
 		addi $a0, $a0, 8 #w (119)
-		# change numbers to fit the process of add and subtract now
-		la $a0, 74 #J (74)
+		syscall
+		sub $a0,$a0,45 #J (74) 119 - 45 = 74
 		syscall
 		addi $a0, $a0, 27 #e (101)
 		syscall
